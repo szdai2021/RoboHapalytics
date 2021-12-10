cf300fc52d5ddadd0abb6d5fdeb093835a32e431
szdai2021 <dai.shaozhang@gmail.com>
2021-12-10 18:32:36 +1100

    12-10-2021

--- Assets/UserStudyControl.cs
+++ Assets/UserStudyControl.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
+using System;
 
 public class UserStudyControl : MonoBehaviour
 {
@@ -10,23 +12,22 @@ public class UserStudyControl : MonoBehaviour
     public GameObject sliderknob;
     public GameObject randomPoint;
 
+    public GameObject sliderKnobReference;
+
     //public WirelessAxes wireless;
     public MadeAxisOscRecieve axisReciever;
     public UnityClient unity_client;
 
     public GameObject robot;
     public GameObject axis;
+    public GameObject axisReference;
 
     public GameObject virtualFingerTouchPoint;
     public GameObject virtualEndEffector;
 
+    public GameObject realSliderReference;
+
     [Header("Parameters")]
-    [HideInInspector] public float virtualKnobMax = 2.4f;
-    [HideInInspector] public float virtualKnobMin = -2.4f;
-
-    [HideInInspector] public float randomPointMax = 0.878f;
-    [HideInInspector] public float randomPointMin = 0.302f;
-
     public int sliderBufferOne;
     public int sliderBufferTwo;
 
@@ -34,19 +35,42 @@ public class UserStudyControl : MonoBehaviour
     public float rangeTwo;
     public float rangeThree;
 
+    public float randomPointOffset;
+
+    public int fileIndex = 0;
+
+    [HideInInspector] public float virtualKnobMax = 2.928f; //2.4f; //2.928
+    [HideInInspector] public float virtualKnobMin = -2.918f; //-2.4f; //-2.918
+
+    [HideInInspector] public float randomPointMax = 0.878f;
+    [HideInInspector] public float randomPointMin = 0.302f;
+
     [Header("")]
     public int scenario = 0; // 0: default;  1: virtual slider with controller;  2: virtual slider with physical 1:1 slider;  3: the haptic slider system
     public bool startFlag = false;
 
+    public bool testFlag = false;
+    public int iterations = 10;
+
     // PRIVATE
     private bool pre_startFlag = false;
-
     private int prev_sliderOne;
+    private bool sFlag = false;
+    private int test_Num;
+    private int rangeIndex = 3;
+
+    // Saved data
+    private List<DateTime> triggerTime = new List<DateTime>();
+    private List<double> reactionTime = new List<double>();
+    private List<double> distanceToTarget = new List<double>();
+    private List<double> distanceToKnob = new List<double>();
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        randomPoint.SetActive(false);
+
+        test_Num = iterations;
     }
 
     // Update is called once per frame
@@ -82,6 +106,7 @@ public class UserStudyControl : MonoBehaviour
                 randomPoint.transform.localPosition = new Vector3(randomPointMin, randomPoint.transform.localPosition.y, randomPoint.transform.localPosition.z);
             }
 
+            sliderKnobReference.transform.position = realSliderReference.transform.position;
 
             switch (scenario) // five different setups
             {
@@ -91,6 +116,7 @@ public class UserStudyControl : MonoBehaviour
                     break;
                 case 3:
                     moveRobot(200, 55, 100, 150);
+                    virtualKnobUpdateFromRobotAxis();
                     break;
                 case 4:
                     break;
@@ -112,26 +138,166 @@ public class UserStudyControl : MonoBehaviour
 
                 unity_client.customMove(RobotCoord.x, RobotCoord.y, RobotCoord.z, RobotRot.x, RobotRot.y, RobotRot.z, movementType: 1);
             }
+
+            if (testFlag)
+            {
+                if (Input.GetKeyDown("s")) // start a new set of experiement
+                {
+                    sFlag = true;
+
+                    triggerTime = new List<DateTime>();
+
+                    reactionTime = new List<double>();
+                    distanceToTarget = new List<double>();
+                    distanceToKnob = new List<double>();
+                }
+
+                if (sFlag & rangeIndex > 0)
+                {
+                    float rangeSelected;
+                    switch (rangeIndex)
+                    {
+                        case 1:
+                            rangeSelected = rangeOne;
+                            break;
+                        case 2:
+                            rangeSelected = rangeTwo;
+                            break;
+                        case 3:
+                            rangeSelected = rangeThree;
+                            break;
+                        default:
+                            rangeSelected = rangeOne;
+                            break;
+                    }
+
+                    if (Input.GetKeyDown("t")) // trigger random point
+                    {
+                        if (triggerTime.Count > 0)
+                        {
+                            distanceToTarget.Add(sliderknob.transform.localPosition.y - randomPoint.transform.localPosition.y);
+                            reactionTime.Add((System.DateTime.Now - triggerTime[triggerTime.Count - 1]).TotalMilliseconds);
+                            test_Num--;
+                        }
+                        generateRandomPoint(rangeSelected);
+                        triggerTime.Add(System.DateTime.Now);
+                    }
+
+                    if (test_Num == 0)
+                    {
+                        rangeIndex--;
+                    }
+
+                    if (rangeIndex == 0)
+                    {
+                        sFlag = false;
+                        test_Num = iterations;
+                        rangeIndex = 3;
+                        randomPoint.SetActive(false);
+                        saveToLocal();
+                    }
+                }
+            }
         }
 
         pre_startFlag = startFlag;
         prev_sliderOne = axisReciever.sliderOne;
     }
 
+    private void saveToLocal()
+    {
+        string saveFileName = "data/test_result" + fileIndex.ToString() +".txt";
+
+        while (File.Exists(saveFileName))
+        {
+            fileIndex++;
+            saveFileName = "data/test_result" + fileIndex.ToString() + ".txt";
+        }
+
+        StreamWriter sw = new StreamWriter(saveFileName);
+
+        sw.WriteLine("Distance to Knob");
+        foreach (var value in distanceToKnob)
+        {
+            sw.WriteLine(value.ToString());
+        }
+        sw.WriteLine(" ");
+
+        sw.WriteLine("Time Taken");
+        foreach (var value in reactionTime)
+        {
+            sw.WriteLine(value.ToString());
+        }
+        sw.WriteLine(" ");
+
+        sw.WriteLine("Accuracy");
+        foreach (var value in distanceToTarget)
+        {
+            sw.WriteLine(value.ToString());
+        }
+
+        sw.Close();
+    }
+
+    private void generateRandomPoint(float rangeDifference)
+    {
+        randomPoint.SetActive(true);
+
+        float randomRangMax = randomPoint.transform.localPosition.y + rangeDifference;
+        float randomRangMin = randomPoint.transform.localPosition.y - rangeDifference;
+
+        float randomValue = UnityEngine.Random.value;
+
+        if (randomRangMax > virtualKnobMax)
+        {
+            randomValue = 0;
+        }
+
+        if (randomRangMin < virtualKnobMin)
+        {
+            randomValue = 1;
+        }
+
+        float randomY = 0;
+        if (randomValue > 0.5)
+        {
+            randomY = UnityEngine.Random.Range(randomRangMax-randomPointOffset, randomRangMax+ randomPointOffset);
+        }
+        else
+        {
+            randomY = UnityEngine.Random.Range(randomRangMin - randomPointOffset, randomRangMin + randomPointOffset);
+        }
+
+        if (randomY > virtualKnobMax)
+        {
+            randomY = virtualKnobMax;
+        }
+
+        if (randomY < virtualKnobMin)
+        {
+            randomY = virtualKnobMin;
+        }
+
+        randomPoint.transform.localPosition = new Vector3(randomPoint.transform.localPosition.x, randomY, randomPoint.transform.localPosition.z);
+
+        distanceToKnob.Add(sliderknob.transform.localPosition.y - randomPoint.transform.localPosition.y);
+
+    }
+
     private void virtualKnobUpdateFromRobotAxis()
     {
-        sliderknob.transform.localPosition = new Vector3(sliderknob.transform.localPosition.x, 1 * (float)(axisReciever.sliderOne - 127) / 255 * (1.89245f - 0.86574f), sliderknob.transform.localPosition.z);
+        sliderknob.transform.localPosition = new Vector3(sliderknob.transform.localPosition.x, sliderKnobReference.transform.localPosition.y + 1 * (float)(axisReciever.sliderOne - 127) / 255 * (1.89245f - 0.86574f), sliderknob.transform.localPosition.z);
     }
 
     private void moveRobot(int bufferOne, int bufferTwo, int bufferThree, int bufferFour)
     {
         if (axisReciever.sliderOne > bufferOne & prev_sliderOne < bufferOne)
         {
-            unity_client.customMove(0.05, 0.4, 0.1, -0.6, 1.5, 0.62, movementType: 1);
+            unity_client.customMove(0.0485766, 0.4551, 0.08486, -0.6, 1.5, 0.62, movementType: 1);
         }
         else if (axisReciever.sliderOne < bufferTwo & prev_sliderOne > bufferTwo)
         {
-            unity_client.customMove(0.35, 0.1, 0.1, -0.6, 1.5, 0.62, movementType: 1);
+            unity_client.customMove(0.4575, 0.0462, 0.088944, -0.6, 1.5, 0.62, movementType: 1);
         }
         else if ((axisReciever.sliderOne > bufferThree & axisReciever.sliderOne < bufferFour) & (prev_sliderOne < bufferThree | prev_sliderOne > bufferFour))
         {
@@ -164,7 +330,7 @@ public class UserStudyControl : MonoBehaviour
         }
     }
 
-    public void HideRobot(bool hideFlag = true)
+    public void HideRobot(bool hideFlag = false)
     {
         bool robotRender = hideFlag;
         bool axisRender = hideFlag;
@@ -182,5 +348,13 @@ public class UserStudyControl : MonoBehaviour
         {
             ar.enabled = axisRender;
         }
+
+        Renderer[] Axis_rsf = axisReference.GetComponentsInChildren<Renderer>();
+
+        foreach (Renderer arf in Axis_rsf)
+        {
+            arf.enabled = axisRender;
+        }
+        
     }
 }
