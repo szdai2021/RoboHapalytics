cce4e540180ec67159ec19e1065e4b0df224945a
szdai2021 <dai.shaozhang@gmail.com>
2022-04-29 17:38:59 +1000

    generated files on date: Fr- 0-/29/2022

--- git-myscript.bat
+++ git-myscript.bat
@@ -1,4 +1,12 @@
+@echo off
+cls
+echo Date format = %date%
+echo dd = %date:~0,2%
+echo mm = %date:~3,2%
+echo yyyy = %date:~6,8%
+echo.
+
 #!/bin/bash
 git status
 git add .
-git commit -m "generated files on `date: '%Y-%m-%d'`'"
+git commit -m "generated files on date: %date:~0,2%-%date:~3,2%-%date:~6,8%"
