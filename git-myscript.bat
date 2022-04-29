5c67de5f626aec7e8164d4f879274108c94a8b1c
szdai2021 <dai.shaozhang@gmail.com>
2022-04-29 17:40:09 +1000

    generated files on date: Fr- 0-/29/2022

--- git-myscript.bat
+++ git-myscript.bat
@@ -5,7 +5,12 @@ echo dd = %date:~0,2%
 echo mm = %date:~3,2%
 echo yyyy = %date:~6,8%
 echo.
-
+echo Time format = %time%
+echo hh = %time:~0,2%
+echo mm = %time:~3,2%
+echo ss = %time:~6,2%
+echo.
+echo Timestamp = %date:~0,2%_%date:~3,2%_%date:~6,8%-%time:~0,2%_%time:~3,2%_%time:~6,2%
 #!/bin/bash
 git status
 git add .
